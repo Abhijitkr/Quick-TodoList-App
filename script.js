c1d9a5aa52e60d0795ff15c066afb4706eafbf4b
Abhijit <abhijitkr600@gmail.com>
2023-09-02 14:15:23 +0530

    Adds check uncheck feature

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const input = document.getElementById('inputTodo');
 const button = document.querySelector('button');
 const ul = document.querySelector('ul');
 
-
-
 button.addEventListener('click', e => {
     e.preventDefault();
-    var inputValue = input.value;
-    var li = document.createElement('li');
+    const li = document.createElement('li');
+    const inputValue = input.value;
     li.innerText = inputValue;
     ul.appendChild(li);
     input.value = '';
+    li.addEventListener('click', () => {
+        li.classList.toggle('checked');
+    }, false);
 }, false);
-
